e5c784cf7af2b346e2b06613d644661d7323426c
LutfiLokman <narxiss@yahoo.com>
2024-01-30 14:11:41 -0500

    auto-push

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Fragment } from "react";
 
-import BarChart from "./components/BarChart";
-import Circle from "./components/Circle";
-import Text from "./components/Text";
+//import BarChart from "./components/BarChart";
+//import Circle from "./components/Circle";
+//import Text from "./components/Text";
+import Drag from "./components/Drag";
 
 function App() {
   return (
     <Fragment>
-      <BarChart />
-      <Circle />
-      <Text />
+     {/*
+       *<BarChart />
+       *<Circle />
+       *<Text />
+       */}
+     <Drag />
     </Fragment>
   );
 }
---
+++ src/components/Drag.jsx
@@ -0,0 +1,62 @@
+import * as d3 from "d3";
+import { Fragment } from "react";
+
+import { useD3 } from "../hooks/useD3";
+
+function Drag() {
+  const ref = useD3((svg) => {
+    let data = [],
+      width = 600,
+      height = 400,
+      numPoints = 10;
+
+    let drag = d3.drag().on("drag", handleDrag);
+
+    function handleDrag(e) {
+      e.subject.x = e.x;
+      e.subject.y = e.y;
+      update();
+    }
+
+    function initDrag() {
+      svg.selectAll("circle").call(drag);
+    }
+
+    function updateData() {
+      data = [];
+      for (let i = 0; i < numPoints; i++) {
+        data.push({
+          id: i,
+          x: Math.random() * width,
+          y: Math.random() * height,
+        });
+      }
+    }
+
+    function update() {
+      svg
+        .selectAll("circle")
+        .data(data)
+        .join("circle")
+        .attr("cx", function (d) {
+          return d.x;
+        })
+        .attr("cy", function (d) {
+          return d.y;
+        })
+        .attr("r", 40);
+    }
+
+    updateData();
+    update();
+    initDrag();
+  }, []);
+
+  return (
+    <Fragment>
+      <svg ref={ref} />
+    </Fragment>
+  );
+}
+
+export default Drag;
